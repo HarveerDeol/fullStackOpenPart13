ffb289cde5bf4098f9a1077cadea80c787d4fce0
Harveer Deol <harveerdeol53@gmail.com>
2025-07-15 12:19:04 -0600

    established connection sucessfully with sequelize

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // ❗️Use false in dev only
+    },
+  },
+});
 
 const main = async () => {
   try {
