9ace50b0001fab1df662ffd3b4be1ebe49ad6c19
Harveer Deol <harveerdeol53@gmail.com>
2025-07-15 13:38:24 -0600

    exercise 13.4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Sequelize, Model, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
@@ -12,7 +12,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 });
-
 
 class Blog extends Model {}
 Blog.init({
@@ -33,7 +32,7 @@ Blog.init({
     allowNull: false
   },
   likes: {
-    type: DataTypes.INT
+    type: DataTypes.INTEGER
   }
 }, {
   sequelize,
@@ -42,19 +41,43 @@ Blog.init({
   modelName: 'blog'
 })
 
+Blog.sync()
+
 app.get('/api/blogs', async (req, res) => {
   const blogs = await Blog.findAll()
-  res.json(notes)
+  console.log(blogs.map(b=>b.toJSON()))
+  res.json(blogs)
 })
+
+app.get('/api/notes/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        console.log(blog.toJSON())
+        res.json(blog)
+    } else {
+        res.status(404).end()
+    }
+    })
 
 app.post('/api/blogs', async (req, res) => {
     try {
       const blog = await Blog.create(req.body)
-      return res.json(note)
+      return res.json(blog)
     } catch(error) {
       return res.status(400).json({ error })
     }
   })
+
+app.put('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        blog.important = req.body.important
+        await blog.save()
+        res.json(blog)
+    } else {
+        res.status(404).end()
+    }
+    })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
